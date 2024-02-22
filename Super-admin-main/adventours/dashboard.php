645ffd1e0773d01e2eb547ccd4e884f07da28810
HanahKeene <147830609+HanahKeene@users.noreply.github.com>
2024-02-22 16:30:05 +0800

    Add files via upload

---
+++ Super-admin-main/adventours/dashboard.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/_dashboard.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <?php require_once './headers.php' ?>
+</head>
+<body>
+    <?php
+    $pageTitle = "Dashboard";
+    require_once './navbar.php';
+    ?>
+    <div class="dashboard">
+        <div class="greetings">
+            <p>Welcome, <b>Trixie Soriano!</b></p>
+        </div>
+        <div class="announcement">
+            <div class="icon">
+                <i class="fa-solid fa-bullhorn"></i>
+            </div>
+            <div class="announcement-txt">
+                <marquee> W E L C O M E  T O  A D V E N T O U R S !!! </marquee>
+            </div>
+            <div class="announce-btn">
+                <button id="myBtn">NEW ANNOUNCEMENT</button>
+                <!-- The __modal -->
+                <div id="my__modal" class="__modal">
+                    <!-- __modal content -->
+                    <div class="__modal-content">
+                        <div class="__modal-header">
+                            <span class="close">&times;</span>
+                            <h2>Add New Announcement</h2>
+                        </div>
+                        <div class="__modal-body">
+                            <input type="text">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="total-counts">
+            <div class="users-box">
+                <p>Total Users</p>
+                <p class="count">200</p>
+            </div>
+            <div class="bookings-box">
+                <p>Active Reservations</p>
+                <p class="count">200</p>
+            </div>
+            <div class="packages-box">
+                <p>Packages</p>
+                <p class="count">200</p>
+            </div> 
+        </div>
+        <?php
+        $text = "USER DEMOGRAPHICS";
+        echo '<p class="left-align">' . $text . '</p>';
+        ?>
+        <?php
+        $title = "12";
+        echo '<div class="rounded-rectangle">';
+        echo '<div class="title">' . $title . '</div>';
+        echo '<p>WOMAN</p>';
+        echo '</div>';
+        ?>
+    </div>
+    <script>
+        // Change title
+        (() => {
+            let state = !0;
+            const changeTitle = () => {
+                document.title = state ? 'Adventours' : 'Admin Dashboard';
+                state = !state;
+            }
+            changeTitle();
+            setInterval(() => {
+                changeTitle()
+            }, 1000)
+        })();
+    </script>
+
+    <script>
+        // Get the __modal
+        var __modal = document.getElementById("my__modal");
+
+        // Get the button that opens the __modal
+        var btn = document.getElementById("myBtn");
+
+        // Get the <span> element that closes the __modal
+        var span = document.getElementsByClassName("close")[0];
+
+        // When the user clicks the button, open the __modal 
+        btn.onclick = function() {
+            __modal.style.display = "block";
+        }
+
+        // When the user clicks on <span> (x), close the __modal
+        span.onclick = function() {
+            __modal.style.display = "none";
+        }
+
+        // When the user clicks anywhere outside of the __modal, close it
+        window.onclick = function(event) {
+            if (event.target == __modal) {
+                __modal.style.display = "none";
+            }
+        }
+    </script>
+</body>
+</html>
---
+++ Super-admin-main/adventours/navbar.php
@@ -0,0 +1,190 @@
+<!-- =============== __navigation ================ -->
+<link rel="stylesheet" href="assets/css/_style.css">
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+</style>
+<style>
+* {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    transition: all 0.3s;
+    font-family: 'Roboto', sans-serif;
+}
+
+.left-align {
+    text-align: left;
+    font-weight: bolder;
+    margin-top: 25px;
+    margin-left: 15px;
+}
+
+.rounded-rectangle {
+    width: 150px;
+    height: 150px;
+    background-color: #ffffff;
+    border: 1px solid #000000;
+    border-radius: 15px;
+    padding: 20px;
+    float: left;
+    margin-top: 20px;
+    margin-left: 20px;
+    font-weight: bolder;
+}
+
+.title {
+    font-size: 20px;
+    margin-bottom: 40px;
+    text-align: center;
+}
+
+li a {
+    display: flex;
+    align-items: start;
+    justify-content: start;
+}
+
+li a .icon {
+    margin-bottom: 20px;
+}
+
+li a .title {
+    margin-top: 5px;
+}
+</style>
+<div class="_container">
+    <div class="__navigation">
+        <ul>
+            <li style="text-align: center;">
+                <a href="#">
+                    <div>
+                        <img src="assets/imgs/5.png" alt="logo" height="200px" width="200px"
+                            style="display: block; margin: 0 auto;">
+                    </div>
+                </a>
+            </li>
+
+
+            <li class="<?= $pageTitle == 'Dashboard' ? 'hovered': null ?>">
+                <a href="./dashboard.php">
+                    <span class="icon">
+                        <ion-icon name="home-outline"></ion-icon>
+                    </span>
+                    <span class="title">Dashboard</span>
+                </a>
+            </li>
+
+            <li class="<?= $pageTitle == 'USER MANAGEMENT' ? 'hovered': null ?>">
+                <a href="./users.php">
+                    <span class="icon">
+                        <ion-icon name="people-outline"></ion-icon>
+                    </span>
+                    <span class="title">Users</span>
+                </a>
+            </li>
+
+            <li class="<?= $pageTitle == 'CONTENT MANAGEMENT' ? 'hovered': null ?>">
+                <a href="./contents.php">
+                    <span class="icon">
+                        <ion-icon name="create"></ion-icon>
+                    </span>
+                    <span class="title">Contents</span>
+                </a>
+            </li>
+
+            <li class="<?= $pageTitle == 'RESERVATION' ? 'hovered': null ?>">
+                <a href="./reservation.php">
+                    <span class="icon">
+                        <ion-icon name="book"></ion-icon>
+                    </span>
+                    <span class="title">Reservations</span>
+                </a>
+            </li>
+
+            <li class="<?= $pageTitle == 'NOTIFICATIONS' ? 'hovered': null ?>">
+                <a href="./notification.php">
+                    <span class="icon">
+                        <ion-icon name="notifications-outline"></ion-icon>
+                    </span>
+                    <span class="title">Notification</span>
+                </a>
+            </li>
+
+            <li class="<?= $pageTitle == 'HELP' ? 'hovered': null ?>">
+                <a href="help.php">
+                    <span class="icon">
+                        <ion-icon name="help-outline"></ion-icon>
+                    </span>
+                    <span class="title">Help</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+
+    <!-- ========================= Main ==================== -->
+    <div class="main">
+        <div class="topbar">
+            <div class="toggle">
+                <ion-icon name="menu-outline"></ion-icon>
+            </div>
+            <div class="page-header">
+                <div class="pagetitle">
+                    <div class="submenu mt-3">
+                        <?php if (isset($pageTitle) && $pageTitle === "USER MANAGEMENT"): ?>
+                        <p>USER MANAGEMENT</p>
+
+                        <?php elseif (isset($pageTitle) && $pageTitle === "RESERVATIONS"): ?>
+                        <p>RESERVATIONS</p>
+
+                        <?php else: ?>
+                        <p><?php echo isset($pageTitle) ? $pageTitle : "DASHBOARD"; ?></p>
+                        <?php endif; ?>
+                        <!-- Repeat the above structure for other page titles and subpages -->
+                    </div>
+                </div>
+
+                <div class="profile p-0 pt-3 px-5">
+                    <i class="fa-solid fa-user" style="color: white;"></i>
+                    <p>ADMINISTRATOR</p><i class="fa-solid fa-caret-down" style="color: white;"></i>
+                    <div class="profile-menu">
+                        <ul>
+                            <li>My Profile</li>
+                            <li>Settings</li>
+                            <li>Logout</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+        <!-- =========== Scripts =========  -->
+
+
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script>
+
+        // return the user to the dashboard if
+        // they are currently logged in
+        if (localStorage.getItem('user_login') != 'true') {
+            location.href = './index.php';
+        }
+
+        function changeSubpageTitle(subpageTitle) {
+            $('#subpageTitle').text(subpageTitle);
+        }
+
+        let toggle = document.querySelector(".toggle");
+        let __navigation = document.querySelector(".__navigation");
+        let main = document.querySelector(".main");
+
+        toggle.onclick = function() {
+            __navigation.classList.toggle("active");
+            main.classList.toggle("active");
+        };
+        </script>
+
+        <!-- ====== ionicons ======= -->
+        <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
